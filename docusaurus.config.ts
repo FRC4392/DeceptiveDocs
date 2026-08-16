a03a54cf12d41c823ca8aab741eeebcfc7fd99e1
kzakfeld <kzakfeld@users.noreply.github.com>
2026-08-16 11:34:04 -0400

    Fixed sidebarId from 'buildsidebar' to 'mediasidebar'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,7 +127,7 @@ const config: Config = {
         },
         {
           type: 'docSidebar',
-          sidebarId: 'buildsidebar',
+          sidebarId: 'mediasidebar',
           position: 'left',
           label: 'Media',
         },
